6abcb9d191df9363fc1622b33d7ff976a801cdf0
carlospelazas <carlospelazas@gmail.com>
2022-11-16 15:10:42 +0100

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,19 @@
+//para poner datos en forma de arreglo
+
+export const tasks = [
+  {
+    id: 0,
+    title: "Tarea 1",
+    description: "mi primera tarea",
+  },
+  {
+    id: 1,
+    title: "Tarea 2",
+    description: "mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "Tarea 3",
+    description: "mi tercera tarea",
+  },
+];
